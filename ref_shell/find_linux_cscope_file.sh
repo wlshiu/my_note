0bb6c8175b9a53978e2a9dedae4e7129636f30aa
wlshiu <wlshiu@mail.com>
2015-06-13 21:45:08 +0800

    add example of find linux interested directory.

---
+++ ref_shell/find_linux_cscope_file.sh
@@ -0,0 +1,18 @@
+#!bin/sh
+
+# LNX=~/linux-3.18.14
+MINGW=F:/_portable/MinGW
+LNX=$MINGW/msys/1.0/home/redegg/linux-3.18.14
+
+cd /
+find  $LNX                            \
+-path "$LNX/arch/arm*" -o             \
+-path "$LNX/include/asm-*" -o         \
+-path "$LNX/tmp*" -prune -o           \
+-path "$LNX/Documentation*" -prune -o \
+-path "$LNX/scripts*" -prune -o       \
+-path "$LNX/drivers*" -prune -o       \
+-path "$LNX/tool*" -prune -o          \
+-path "$LNX/sound*" -prune -o         \
+-path "$LNX/lib*" -prune -o           \
+-name "*.[ch]" > $LNX/cscope.files
