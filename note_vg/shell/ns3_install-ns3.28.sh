79f47fc3ca8b4aee3c51ffdf94213a5d3d3aa251
Wei-Lun <wl@mail.com>
2018-12-12 22:18:51 +0800

    update

--- note_vg/shell/ns3_install-ns3.28.sh
+++ note_vg/shell/ns3_install-ns3.28.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 Red='\e[0;31m'
 Yellow='\e[1;33m'
@@ -11,11 +11,28 @@ set -e
 
 version=3.28
 
-# sudo apt-get update
-sudo apt-get -y install gcc g++
+if [ ${version} == 3.28 ]; then
+    sudo apt-get -y install software-properties-common
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+    sudo apt-get update
+    sudo apt-get -y install gcc-4.9 g++-4.9
+
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 49 \
+        --slave /usr/bin/g++ g++ /usr/bin/g++-4.9 \
+        --slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-4.9 \
+        --slave /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-4.9 \
+        --slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-4.9
+
+    sudo apt-get -y install qt5-default
+
+elif [ ${version} == 3.26 ]; then
+    # use gcc-4.8, g++-4.8
+    sudo apt-get update
+    sudo apt-get -y install gcc g++
+    sudo apt-get -y install qt4-dev-tools libqt4-dev
+fi
+
 sudo apt-get -y install python python-dev
-# sudo apt-get -y install qt4-dev-tools libqt4-dev
-sudo apt-get -y install qt5-default
 sudo apt-get -y install mercurial
 sudo apt-get -y install bzr
 sudo apt-get -y install cmake libc6-dev libc6-dev-i386 g++-multilib
