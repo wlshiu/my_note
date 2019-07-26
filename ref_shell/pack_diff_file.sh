57483fe85458e6fb75e2dda76485db78dda06b5c
Wei-Lun <wl@mail.com>
2019-07-27 07:38:15 +0800

    update pack_diff

--- ref_shell/pack_diff_file.sh
+++ ref_shell/pack_diff_file.sh
@@ -2,9 +2,21 @@
 
 output_file=${PWD##*/}.tar
 
-tar -cf $output_file `git diff --stat --name-only test/master | awk '{print $1}'`
+help()
+{
+    echo -e "usage: $0 [branch name] [move to path]"
+    exit 1;
+}
+
+if [ $# == 0 ]; then
+    help
+fi
+
+cmp_branch_name=$1
+
+tar -cf $output_file `git diff --stat --name-only ${cmp_branch_name} | awk '{print $1}'`
 
 if [ -n "$2" ]; then
-    mv -i  $output_file $2
+    mv -i $output_file $2
 fi
 
