0b0ee5f22f7a99860c6b468bb86f3bc7dade0db6
Wei-Lun <wl@mail.com>
2019-09-26 20:50:19 +0800

    update

---
+++ note_vg/shell/repo_push.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#
+# repo_push is used to commit code with code review server (e.g. github, gitlab)
+#
+
+set -e 
+
+#
+# get the local branch name (repo start [loacl name] --all)
+#
+
+# repo status 
+
+#
+# get the project list
+#
+
+
+#
+# get the branch name of projects from remote
+#
+
+# repo info | grep xxx
+
+
+#
+# use 'git push' to the target branch
+#
+
+# cd xxx
+# git push origin local:[act_branch]
+
+
+#=====================================================
+# Counting objects: 5, done.
+# Delta compression using up to 2 threads.
+# Compressing objects: 100% (2/2), done.
+# Writing objects: 100% (3/3), 293 bytes, done.
+# Total 3 (delta 0), reused 0 (delta 0)
+# To ssh://git@github.com/ossxp-com/test1.git
+   # 27aee23..4f94123  jiangxin -> master
+# Counting objects: 5, done.
+# Writing objects: 100% (3/3), 261 bytes, done.
+# Total 3 (delta 0), reused 0 (delta 0)
+# To ssh://git@github.com/ossxp-com/test2.git
+   # 7f0841d..86683ec  jiangxin -> master
+
+# --------------------------------------------
+# [OK    ] test/test1/     jiangxin
+# [OK    ] test/test2/     jiangxin
