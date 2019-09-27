37da730909c0dfa185b9825fb4733f1558533348
Wei-Lun <wlshiu913@gmail.com>
2019-09-27 11:15:02 +0800

    Update repo_push.sh

--- note_vg/shell/repo_push.sh
+++ note_vg/shell/repo_push.sh
@@ -4,48 +4,59 @@
 # repo_push is used to commit code with code review server (e.g. github, gitlab)
 #
 
-set -e 
+Red='\e[0;31m'
+Yellow='\e[1;33m'
+Green='\e[0;32m'
+NC='\e[0m'
 
-#
-# get the local branch name (repo start [loacl name] --all)
-#
+# set -e
 
-# repo status 
+result=""
 
-#
-# get the project list
-#
+push_code()
+{
+    proj_path=$1
+    local_branch=$2
+    remote_branch=$3
 
+    git push origin ${local_branch}:${remote_branch}
+    # echo -e "push origin ${local_branch}:${remote_branch}"
 
-#
-# get the branch name of projects from remote
-#
+    if [ $? != 0 ]; then
+        result="${result}[${Red}FAIL  ${NC}] ${proj_path}:${Green}${local_branch} -> ${remote_branch}${NC}\n"
+    else
+        result="${result}[OK    ] ${proj_path}:${Green}${local_branch} -> ${remote_branch}${NC}\n"
+    fi
+}
 
-# repo info | grep xxx
+tmp_file=___tmp
+project_info=___proj_info
+project_list=___proj_list
 
+repo info > ${project_info}
+repo status > ${project_list}
 
-#
-# use 'git push' to the target branch
-#
+cur_dir=$(pwd)
+patterns=($(cat ${project_list} | grep '.*project.*branch' | awk '{print $2}' | sed 's:\./::' | sed 's:/$::'))
 
-# cd xxx
-# git push origin local:[act_branch]
+for patt in "${patterns[@]}"; do
+    echo -e ${Yellow}'\nproject' ${patt} '...' ${NC}
+    grep -A 2 ${patt} ${project_info} > ${tmp_file}
 
+    # get the branch name of projects from remote
+    revision=$(cat ${tmp_file} | grep 'Current revision:' | awk -F ": " '{print $2}')
+    l_branch=$(cat ${tmp_file} | grep 'Local Branches:' | awk -F ":" '{print $2}' | awk -F " " '{print $2}' | sed 's:\[::' | sed 's:\]::')
+    rm -f ${tmp_file}
 
-#=====================================================
-# Counting objects: 5, done.
-# Delta compression using up to 2 threads.
-# Compressing objects: 100% (2/2), done.
-# Writing objects: 100% (3/3), 293 bytes, done.
-# Total 3 (delta 0), reused 0 (delta 0)
-# To ssh://git@github.com/ossxp-com/test1.git
-   # 27aee23..4f94123  jiangxin -> master
-# Counting objects: 5, done.
-# Writing objects: 100% (3/3), 261 bytes, done.
-# Total 3 (delta 0), reused 0 (delta 0)
-# To ssh://git@github.com/ossxp-com/test2.git
-   # 7f0841d..86683ec  jiangxin -> master
+    cd ${patt}
 
-# --------------------------------------------
-# [OK    ] test/test1/     jiangxin
-# [OK    ] test/test2/     jiangxin
+    push_code ${patt} ${l_branch} ${revision}
+    cd ${cur_dir}
+
+done
+
+echo -e "\n--------------------------------------------"
+echo -e ${result}
+
+rm -f ${project_info}
+rm -f ${project_list}
