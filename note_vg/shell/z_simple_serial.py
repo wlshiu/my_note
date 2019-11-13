377120484d23990cc374d4f3966d34b90db9fe24
Wei-Lun <wlshiu913@gmail.com>
2019-11-13 13:36:15 +0800

    Update z_simple_serial.py

--- note_vg/shell/z_simple_serial.py
+++ note_vg/shell/z_simple_serial.py
@@ -4,30 +4,43 @@
 import serial
 import time
 import sys
+import Queue as queue
+import re
 
 SERIAL_PORT = '/dev/ttyS0'
 BAUD_RATES = 115200
-ser = serial.Serial(SERIAL_PORT, BAUD_RATES, timeout=0)
+# ser = serial.Serial(SERIAL_PORT, BAUD_RATES, timeout=0)
+ser = serial.Serial(SERIAL_PORT, BAUD_RATES)
 
-def main():
-    is_wait = True
+def readLine():
+    line = ''
     while True:
-        for i in range(1, 3):
-            count = ser.inWaiting()
-            if count != 0:
-                is_wait = False
-                recv = ser.read(count)
-                sys.stdout.write(recv)
-                sys.stdout.flush()
-                #ser.write(recv)
+        time.sleep(0.1)
+        count = ser.inWaiting()
+        if count:
+            for i in range(1, count):
+                ch = ser.read(1)
+                line += ch
+                if ch == '\n':
+                    sys.stdout.write(line)
+                    sys.stdout.flush()
+                    return line
 
             ser.flushInput()
-            # time.sleep(0.5)
-            time.sleep(1)
 
-            if is_wait == False:
-                # tx_data = str(i) + '\r\n'
-                ser.write(str(i) + '\r\n') # send '2' and press enter '\r'
+
+def main():
+    do_send = False
+    index = 1
+    item_num = 0
+    while True:
+        rx_line = readLine()
+        # TODO: parse received line string
+
+        if do_send == True:
+            # tx_data = str(i) + '\r\n'
+            # ser.write(str(index) + '\r\n') # send '2' and press enter '\r'
+            index += 1
 
 if __name__ == '__main__':
     try:
