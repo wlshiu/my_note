eca690c3ea6fc714c751d92e9121d2ce0dbefb78
Wei-Lun <wl@mail.com>
2020-12-12 11:55:05 +0800

    update

--- note_icm/bash/z_base32.sh
+++ note_icm/bash/z_base32.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Copyright (c) 2020, All Rights Reserved.
+# @file    z_base32.sh
+# @author  Wei-Lun Hsu
+# @version 0.1
+
 
 Red='\e[0;31m'
 Yellow='\e[1;33m'
@@ -15,21 +20,21 @@ bin_raw=$4
 
 help()
 {
-    echo -e "usage: $0 <-d/-e> <input-path> <output-path> [bin]"
+    echo -e "usage: $0 <-d/-e> <input-path> <output-path> <bin>"
     exit 1;
 }
 
 converter()
 {
-    name=$(echo $1 | base32 | sed 's:=:@:g')
+    local name=$(echo $1 | base32 | sed 's:=:@:g')
     cat $1 > $out_dir/${name}
 }
 
 converter2()
 {
-    item=$(basename $1)
-    path=$(echo $item | sed 's:@:=:g' | base32 -d)
-    out=$(dirname $path)
+    local item=$(basename $1)
+    local path=$(echo $item | sed 's:@:=:g' | base32 -d)
+    local out=$(dirname $path)
 
     if [ ! -d "$out_dir/$out" ]; then
         mkdir -p $out_dir/$out
@@ -38,31 +43,71 @@ converter2()
     cp -f $in_dir/$item $out_dir/$path
 }
 
+search_target_input()
+{
+    echo -e $1
+    local item=$(echo $1 | sed 's:'"${out_dir}"'::')
+    if [ ! -z $item ]; then
+        target_input=${out_dir}${item};
+    fi
+}
+
+
 export -f converter
 export -f converter2
+export -f search_target_input
+
+
+if [ $# != 4 ]; then
+    help
+fi
 
 case $1 in
     "-d")
-        if [ $# != 3 ]; then
-            help
+        if [[ -d "${out_dir}" ]]; then
+            echo -e "${Red} output folder exist ...${NC}"
+            exit 1;
         fi
 
-        find ${in_dir} -type f -exec bash -c 'converter2 "{}"' \;
+        mkdir -p ${out_dir}
 
-        # unzip -q ${bin_raw}
+        # unzip -d ${out_dir} -q ${bin_raw}
+        openssl enc -d -aes256 -in ${bin_raw} | tar zx -C ${out_dir}
+
+        verify_cnt=$(ls ${out_dir} -l | grep '^d' | wc -l)
+        if [ ${verify_cnt} != 1 ]; then
+            echo -e "${Red} unknown directory architecture...${NC}"
+            exit 1;
+        fi
+
+        find ${out_dir} -maxdepth 1 -type d -print | sed 's:'"${out_dir}"'::' > ___tmp
+        sed -i '/^$/d' ___tmp
+        in_dir=${out_dir}$(cat ___tmp)
+        rm -f ___tmp
+
+        find ${in_dir} -type f -exec bash -c 'converter2 "{}"' \;
+        echo "done~~~"
         ;;
 
     "-e")
-        if [ $# != 4 ]; then
-            help
+        if [[ -d "${out_dir}" ]]; then
+            echo -e "${Red} output folder exist ...${NC}"
+            exit 1;
         fi
 
-        find ${in_dir} -type f -name '*.c' -exec bash -c 'converter "{}"' \;
-        find ${in_dir} -type f -name '*.h' -exec bash -c 'converter "{}"' \;
-        find ${in_dir} -type f -iname '*.s' -exec bash -c 'converter "{}"' \;
-        find ${in_dir} -type f -iname '*.mk' -exec bash -c 'converter "{}"' \;
+        mkdir -p ${out_dir}
 
-        zip -r -e ${bin_raw} ${out_dir} -q
+        if [ -f ${in_dir} ]; then
+            bash -c 'converter "${in_dir}"'
+        else
+            find ${in_dir} -type f -name '*.c' -exec bash -c 'converter "{}"' \;
+            find ${in_dir} -type f -name '*.h' -exec bash -c 'converter "{}"' \;
+            find ${in_dir} -type f -iname '*.s' -exec bash -c 'converter "{}"' \;
+            find ${in_dir} -type f -iname '*.mk' -exec bash -c 'converter "{}"' \;
+        fi
+
+        # zip -r -9 -e ${bin_raw} ${out_dir} -q
+        tar -zcpf - ${out_dir}/* | openssl enc -e -aes256 -out ${bin_raw}
         ;;
 
     *)
@@ -70,3 +115,4 @@ case $1 in
         ;;
 
 esac    # --- end of case ---
+
