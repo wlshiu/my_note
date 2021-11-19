02936cba22cd09dc4c826086a98be8574eafd54c
Wei-Lun <wlshiu913@gmail.com>
2021-11-19 09:02:47 +0800

    Update z_auto_build_keil_proj.sh

--- note_zbit/shell/z_auto_build_keil_proj.sh
+++ note_zbit/shell/z_auto_build_keil_proj.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+Red='\e[0;31m'
+Yellow='\e[1;33m'
+Light_Gray='\e[0;37m'
+NC='\e[0m' # No Color
+
+if [ $# != 2 ]; then
+    echo -e "usage: $0 <keil project path> <log directory path>"
+    exit -1
+fi
+
 KEIL_MDK="/C/Keil_v5/UV4:/C/Keil_v5/ARM/ARMCC/bin"
 
 env_setup_file=__env.sh
@@ -11,17 +21,23 @@ source $env_setup_file
 rm -f $env_setup_file
 
 cur_dir=$(pwd)
+log_dir=$2
 
 keil_proj=$1
 build_log=$(echo "$keil_proj" | sed -e 's/\//@/g')
 build_log=$build_log.log
 
+echo -e "$Yellow build $1 ... $NC"
+
 UV4.exe -j0 -cr $keil_proj -o $cur_dir/$build_log
 
 err_cnt=$(grep -e '- [0-9+] Error(s), [0-9+] Warning(s)[.]' $cur_dir/$build_log | awk -F" Error" '{print $1}' | awk -F"- " '{print $(NF)}')
 
-if [ $err_cnt != 0 ]; then
-    echo -e "build fail: $keil_proj"
+sleep 1
+
+if [ "$err_cnt" != "0" ]; then
+    echo -e "$Red build fail: $keil_proj $NC"
+    mv -f $cur_dir/$build_log $log_dir/$build_log
 else
     rm -f $cur_dir/$build_log
 fi
