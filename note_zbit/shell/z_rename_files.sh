928afda959b959c3dfc9e66b01353c04b0b3eb70
Wei-Lun <wlshiu913@gmail.com>
2023-03-22 12:19:48 +0800

    Update z_rename_files.sh

--- note_zbit/shell/z_rename_files.sh
+++ note_zbit/shell/z_rename_files.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-patt=ttt
-new_patt=ccc
+
+help()
+{
+    echo -e "usage: $0 <pattern> <target>"
+    echo -e "   $0 ttt ccc"
+    echo -e "       rename file: aa/k/ttt_foo.c -> aa/k/ccc_foo.c"
+    exit -1
+}
+
+if [ $# != 2 ]; then
+    help
+fi
+
+patt=$1
+new_patt=$2
+
 
 #
 # rename file: ttt_foo.c -> ccc_foo.c
 #
 
-find . -type f -name 'ttt_*' | while read FILE ; do
-    newfile="$(echo ${FILE} | sed -e 's/ttt/ccc/')" ;
+find . -type f -name "${patt}*" | while read FILE ; do
+    newfile="$(echo ${FILE} | sed -e 's/'$patt'/'$new_patt'/')" ;
+    echo -e "${FILE}"
+    echo -e "  => ${newfile}"
+    mv "${FILE}" "${newfile}" ;
 
-    # mv "${FILE}" "${newfile}" ;
-    echo ${newfile}
 done
